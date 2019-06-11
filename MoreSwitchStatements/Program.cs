51737dc67e0490c37bcb3c25fb50147db3f6f16d
RAlanWright <wright.alan88@gmail.com>
2019-06-11 18:30:56 -0500

    Added user input functionality for the case options

--- MoreSwitchStatements/Program.cs
+++ MoreSwitchStatements/Program.cs
@@ -10,6 +10,27 @@ namespace MoreSwitchStatements
     {
         static void Main(string[] args)
         {
+            string choice;
+            Console.WriteLine("What color car would you like? ");
+            choice = Console.ReadLine();
+
+            checkColor(choice);
+        }
+        public static void checkColor(string choice)
+        {
+            string carColor;
+            switch (choice)
+            {
+                case "red":
+                case "blue":
+                case "yellow":
+                    carColor = $"We have a {choice} car!";
+                    break;
+                default:
+                    Console.WriteLine("Sorry, we don't have a car in this color..");
+                    return;
+            }
+            Console.WriteLine(carColor);
         }
     }
 }
